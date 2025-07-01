d85e2f5f3c40e81ed374ba3f6ee7f0e3e9386a95
Abel Eyasu <abeleyasu98@gmail.com>
2025-07-02 00:52:08 +0300

    header height from the top

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./logo";
 
 const Header = () => {
   return (
-    <div className="container flex h-[60px] shrink-0 items-center justify-center px-4 lg:h-[80px] lg:px-0">
+    <div className="container flex h-[90px] shrink-0 items-center justify-center px-4 lg:h-[110px] lg:px-0">
       <a href="/">
         <Logo className="w-300 sm:w-360" />
       </a>
