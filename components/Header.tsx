6268ae9853de3b5be84692fb22b7b6343bbe1930
Abel Eyasu <abeleyasu98@gmail.com>
2025-07-02 00:48:08 +0300

    logo size

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="container flex h-[60px] shrink-0 items-center justify-center px-4 lg:h-[80px] lg:px-0">
       <a href="/">
-        <Logo className="w-30 sm:w-36" />
+        <Logo className="w-300 sm:w-360" />
       </a>
     </div>
   );
