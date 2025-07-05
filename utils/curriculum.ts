83fc1bb87de92dd66ce66f4c0d583acf978be129
Abel Eyasu <abeleyasu98@gmail.com>
2025-07-05 13:28:16 +0300

    curriculum ts

---
+++ utils/curriculum.ts
@@ -0,0 +1,77 @@
+// utils/curriculum.ts
+
+export const curriculumData = [
+  {
+    grade: "Grade 9",
+    subjects: [
+      {
+        name: "Biology",
+        units: [
+          {
+            unit: "Unit 1: Cell Biology",
+            subunits: [
+              "Sub-U1.1: The Cell",
+              "Sub-U1.2: Cell Structure",
+              "Sub-U1.3: Cell Function",
+            ],
+          },
+          {
+            unit: "Unit 2: Genetics",
+            subunits: ["Sub-U2.1: DNA", "Sub-U2.2: Heredity"],
+          },
+        ],
+      },
+      {
+        name: "Physics",
+        units: [
+          {
+            unit: "Unit 1: Motion",
+            subunits: ["Sub-U1.1: Speed", "Sub-U1.2: Acceleration"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    grade: "Grade 10",
+    subjects: [
+      {
+        name: "Chemistry",
+        units: [
+          {
+            unit: "Unit 1: Atomic Structure",
+            subunits: ["Sub-U1.1: Atoms", "Sub-U1.2: Isotopes"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    grade: "Grade 11",
+    subjects: [
+      {
+        name: "Math",
+        units: [
+          {
+            unit: "Unit 1: Functions",
+            subunits: ["Sub-U1.1: Linear", "Sub-U1.2: Quadratic"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    grade: "Grade 12",
+    subjects: [
+      {
+        name: "Physics",
+        units: [
+          {
+            unit: "Unit 1: Thermodynamics",
+            subunits: ["Sub-U1.1: Heat", "Sub-U1.2: Energy Transfer"],
+          },
+        ],
+      },
+    ],
+  },
+];
